c6c99e6a4e363b83e3c969a54437c0c02ed9943e
Michael <michaelk@vimeo.com>
2017-09-25 17:32:46 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Transaction struct {
+	Date   time.Time
+	Place  string
+	Amount float64
+}
+
+type Transactions struct {
+	Transactions    []Transaction
+	Max             float64
+	Min             float64
+	Average         float64
+	Sum             float64
+	NumTransactions int
+	Places          []string
+	PlacesMap       map[string]int
+	Dates           []string
+	DatesMap        map[string]int
+}
+
+const TIME_LAYOUT = "01/01/2017"
+
+func main() {
+
+	// open the file for read purposes
+	file, err := os.OpenFile("./transactions.csv", os.O_RDONLY, 0600)
+	if err != nil {
+		fmt.Println("Error opening file")
+		os.Exit(0)
+	}
+
+	// Create a scanner over the file
+	scanner := bufio.NewScanner(file)
+
+	// declare variables we need
+	var transactions []Transaction
+	var places []string
+	var dates []string
+	min := 10000.0
+	max := -1.0
+	sum := 0.0
+	placesMap := make(map[string]int)
+	datesMap := make(map[string]int)
+
+	// File should be in the form:
+	// Date,Place,Amount
+	// Ex: 09/25/2017,Market Street,24.30
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, ",")
+		date := parts[0]
+		place := parts[1]
+		amount, _ := strconv.ParseFloat(parts[2], 64)
+		dateObj, _ := time.Parse(date, TIME_LAYOUT)
+
+		if amount < min {
+			min = amount
+		}
+
+		if amount > max {
+			max = amount
+		}
+
+		sum = sum + amount
+		places = append(places, place)
+		dates = append(dates, date)
+
+		// use this to get more accurate count
+		// p := place[0:5]
+
+		// maps
+		if placesMap[place] == 0 {
+			placesMap[place] = 0
+		}
+
+		if datesMap[date] == 0 {
+			datesMap[date] = 0
+		}
+
+		placesMap[place] = placesMap[place] + 1
+		datesMap[date] = datesMap[date] + 1
+
+		transactions = append(transactions, Transaction{
+			Date:   dateObj,
+			Place:  place,
+			Amount: amount,
+		})
+	}
+
+	avg := float64(int(sum) / len(transactions))
+	t := Transactions{
+		Transactions:    transactions,
+		Min:             min,
+		Max:             max,
+		Average:         avg,
+		Places:          places,
+		PlacesMap:       placesMap,
+		DatesMap:        datesMap,
+		Dates:           dates,
+		Sum:             sum,
+		NumTransactions: len(transactions),
+	}
+
+	// print quick stats
+	fmt.Println("==>Num: ", t.NumTransactions)
+	fmt.Println("==>Sum: ", t.Sum)
+	fmt.Println("==>Max: ", t.Max)
+	fmt.Println("==>Min: ", t.Min)
+	fmt.Println("==>Avg: ", t.Average)
+
+	fmt.Println("==>Places:<==")
+	// print other stats
+	for place, count := range t.PlacesMap {
+		fmt.Println(place, ": ", count)
+	}
+
+	file.Close()
+}
